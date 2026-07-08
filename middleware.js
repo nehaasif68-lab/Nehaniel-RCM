5e107aa7eb787ff95a299a4fd3a6d375b1cc2aa6
nehaasif68-lab <nehaasif68@gmail.com>
2026-07-08 17:39:36 -0600

    middleware.js

--- middleware.js
+++ middleware.js
@@ -1,9 +1,36 @@
+import { createServerClient } from "@supabase/ssr";
 import { NextResponse } from "next/server";
 
-export function middleware() {
-  return NextResponse.next();
+export async function middleware(request) {
+  let response = NextResponse.next();
+
+  const supabase = createServerClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+    {
+      cookies: {
+        getAll() {
+          return request.cookies.getAll();
+        },
+        setAll(cookiesToSet) {
+          cookiesToSet.forEach(({ name, value }) => {
+            response.cookies.set(name, value);
+          });
+        },
+      },
+    }
+  );
+
+  await supabase.auth.getUser();
+
+  return response;
 }
 
 export const config = {
-  matcher: [],
+  matcher: [
+    "/login",
+    "/orgs/:path*",
+    "/dashboard/:path*",
+    "/update-password"
+  ],
 };
